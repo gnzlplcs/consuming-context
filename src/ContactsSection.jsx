fdbcaf094fddadf10e0ad40b96df9dd8744f5861
gnzlplcs <palaciosgm@outlook.com>
2022-08-03 14:27:18 -0500

    5️⃣ step five completed

--- src/ContactsSection.jsx
+++ src/ContactsSection.jsx
@@ -1,11 +1,13 @@
+// In ContactsSection.js, remove the unused prop drilling.
+
 import React from "react";
 import { ContactsList } from "./ContactsList";
 
-export const ContactsSection = ({ contacts, name, theme }) => {
+export const ContactsSection = ({ contacts, name }) => {
   return (
     <div>
       <h2>{name}</h2>
-      <ContactsList contacts={contacts} theme={theme} />
+      <ContactsList contacts={contacts} />
     </div>
   );
 };
