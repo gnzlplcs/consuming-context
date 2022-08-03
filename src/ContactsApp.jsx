373330eb8af9f7a3fd58d55c3d16d46e57e87998
gnzlplcs <palaciosgm@outlook.com>
2022-08-03 14:20:12 -0500

    2️⃣ step two completed

--- src/ContactsApp.jsx
+++ src/ContactsApp.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { ContactsSection } from "./ContactsSection";
+import { ThemeContext } from "./ThemeContext";
 import './index.css';
+
+// Now that you’ve created a ThemeContext, import it into the ContactsApp.js file. In the ContactsApp function component, wrap the existing returned children inside a <ThemeContext.Provider> component with a value prop set to "light".
 
 const family = [
   {
@@ -22,10 +25,12 @@ const friends = [
 
 export const ContactsApp = () => {
   return (
-    <div>
-      <h1>Contacts</h1>
-      <ContactsSection contacts={family} name="Family" theme="dark" />
-      <ContactsSection contacts={friends} name="Friends" theme="dark" />
-    </div>
+    <ThemeContext.Provider value="light">
+      <div>
+        <h1>Contacts</h1>
+        <ContactsSection contacts={family} name="Family" theme="dark" />
+        <ContactsSection contacts={friends} name="Friends" theme="dark" />
+      </div>
+    </ThemeContext.Provider>
   );
 };
