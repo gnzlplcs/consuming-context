14fb7b00a0468869600617f14e7ff9c548aaecf4
gnzlplcs <palaciosgm@outlook.com>
2022-08-03 14:17:26 -0500

    1️⃣ step one completed

---
+++ src/ThemeContext.jsx
@@ -0,0 +1,5 @@
+// In the workspace’s ThemeContext.js file, use React.createContext() to create a new context. Then, store the new context in a variable named ThemeContext. Finally, export ThemeContext as a named export.
+
+import React from 'react';
+
+export const ThemeContext = React.createContext();
