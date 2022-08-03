13d4032f67e273ed9a0ab75bcca16342059feb2b
gnzlplcs <palaciosgm@outlook.com>
2022-08-03 14:23:10 -0500

    3️⃣ step three completed

--- src/ContactItem.jsx
+++ src/ContactItem.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+// In the ContactItem.js file, remove the theme prop from the ContactItem component’s prop list. Have it use React’s useContext() hook to retrieve the ThemeContext’s provided theme value instead.
 
-export const ContactItem = ({ name, theme }) => {
+import React, { useContext } from "react";
+import { ThemeContext } from "./ThemeContext";
+
+export const ContactItem = ({ name }) => {
+  const theme = useContext(ThemeContext)
+
   return (
     <div className={`theme-${theme}`}>
       {name} {theme === "dark" ? "🌑" : "☀"}
