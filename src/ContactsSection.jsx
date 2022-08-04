f66a4ae392921b02f1c250e545125e4efb2a1f43
gnzlplcs <palaciosgm@outlook.com>
2022-08-04 17:52:54 -0500

    add comments

--- src/ContactsSection.jsx
+++ src/ContactsSection.jsx
@@ -3,6 +3,7 @@ import { ContactsList } from "./ContactsList";
 import ThemeSwitcher from "./ThemeSwitcher";
 
 export const ContactsSection = ({ contacts, name }) => {
+  // Import the ThemeSwitcher component into ContactsSection.js. Render it as a child of the ContactsSection component, immediately after the h2.
   return (
     <div>
       <h2>{name}</h2>
--- src/ThemeSwitcher.jsx
+++ src/ThemeSwitcher.jsx
@@ -2,9 +2,13 @@ import React, { useContext } from "react";
 import { ThemeContext } from "./ThemeContext";
 
 const ThemeSwitcher = () => {
+  // ThemeSwitcher should retrieve both setTheme and theme from ThemeContext as variables.
   const { theme, setTheme } = useContext(ThemeContext);
 
   return (
+    // It should also render a button whose text content is Theme is currently: {theme}.
+
+    // In the ThemeSwitcher component, add an onClick callback on the <button>. It should call setTheme with "dark" if the current theme is "light", or "light" if the current theme is "dark".
     <button
       onClick={() => setTheme((prev) => (prev === "dark" ? "light" : "dark"))}
     >
